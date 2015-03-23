de4832d2adca428d5c51d336bb4f55a89b790d96
pote <pote@tardis.com.uy>
2015-03-23 00:22:44 -0300

    Trim down crap

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import(
+  "code.google.com/p/go-uuid/uuid"
+  "errors"
   "github.com/garyburd/redigo/redis"
   "github.com/pote/chronicler"
   "github.com/pote/redisurl"
   "golang.org/x/net/websocket"
   "net/http"
+  "os"
   "log"
-  "routes"
   "runtime"
+  "strings"
 )
 
 var RedisPool *redis.Pool = SetupRedis()
@@ -26,13 +29,64 @@ func main() {
   runtime.GOMAXPROCS(runtime.NumCPU())
   log.Printf("[Main] Initializing Philotic Network on %v core(s)\n", runtime.NumCPU())
 
-
   http.Handle("/", websocket.Handler(ServeWebSocket))
-  go http.ListenAndServe(":8181", nil)
-
-  web := chronicler.NewStory()
-  web.Register(&routes.Root{})
-
-  web.Serve(":8080")
+  http.ListenAndServe(":" + os.Getenv("PORT"), nil)
 }
 
+func ServeWebSocket(ws *websocket.Conn) {
+  if _, _, err := RoutingInfo(ws.Request().URL.Path); err != nil {
+    log.Fatal(err)
+    ws.Close()
+  }
+  identifier := uuid.New()
+
+  go ReceiveMessages(identifier, ws)
+  go DispatchMessages(identifier, ws)
+  select{}
+}
+
+func DispatchMessages(identifier string, ws *websocket.Conn) {
+  _, channel, _ := RoutingInfo(ws.Request().URL.Path)
+  pubSub := redis.PubSubConn{Conn: RedisPool.Get()}
+  pubSub.PSubscribe(channel + ":*")
+
+  for {
+    switch m := pubSub.Receive().(type) {
+    case redis.PMessage:
+      if m.Channel != channel + ":" + identifier {
+        websocket.Message.Send(ws, string(m.Data))
+      }
+    }
+  }
+}
+
+func ReceiveMessages(identifier string, ws *websocket.Conn) {
+  _, channel, _ := RoutingInfo(ws.Request().URL.Path)
+
+  for {
+    var message string
+    websocket.Message.Receive(ws, &message)
+
+    c := RedisPool.Get()
+    c.Do("PUBLISH", channel + ":" + identifier, message)
+    c.Close()
+  }
+}
+
+func RoutingInfo(path string) (org, channel string, err error) {
+  sections := strings.Split(path, "/")
+  if len(sections) < 3 {
+    err = errors.New("Connection needs to request an org/channel pair in path")
+    return
+  }
+
+  routingInfo := []string{
+    sections[1],
+    sections[2],
+  }
+
+  org         = sections[1]
+  channel     = strings.Join(routingInfo, ":")
+
+  return
+}
