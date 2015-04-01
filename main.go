e0c452445218be132aea1fe1c3abf8f8ad7624fc
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2015-04-01 00:01:22 -0300

    Simplify JSON representations
    Don't send empty fields when propagating Messages around Redis. This
    keeps our payloads small.

--- main.go
+++ main.go
@@ -53,9 +53,9 @@ func ServeWebSocket(ws *websocket.Conn) {
 }
 
 type Message struct {
-	UUID    string `json:"id"`
-	Channel string `json:"channel"`
-	Data    string `json:"data"`
+	UUID    string `json:"id,omitempty"`
+	Channel string `json:"channel,omitempty"`
+	Data    string `json:"data,omitempty"`
 	Event   string `json:"event,omitempty"`
 }
 
