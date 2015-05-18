bd3d9ac0805f7eb85567aecdf6bf9a58df021abe
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2015-05-18 21:12:04 +0100

    Fix the tests

--- access_token_test.go
+++ access_token_test.go
@@ -3,9 +3,16 @@ package main
 import "testing"
 
 func TestParseAccessToken(t *testing.T) {
+	redis := RedisPool.Get()
+	defer redis.Close()
+
+	redis.Do("SET", "hubs:deadbeef", "deadbeefsecret")
+
 	var tokenString = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJodWIiOiJkZWFkYmVlZiIsImNoYW5uZWxzIjpbImNoYXQiLCJ1cGRhdGVzIl19.vlqHRxfxSidyH9_oW-rMl_LvLR8UqhK5uGc5KRjTxl0"
 
 	token, err := ParseAccessToken(tokenString)
+
+	redis.Do("DEL", "hubs:deadbeef")
 
 	if err != nil {
 		t.Error(err)
