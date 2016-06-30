9c209ec8e3e2f9491b6ce12644561a573c99a288
pote <pote@tardis.com.uy>
2016-06-30 17:22:22 -0300

    Tests client writing on non-permitted channel

--- socket_test.go
+++ socket_test.go
@@ -143,3 +143,66 @@ func TestChannelIsolation (t *testing.T) {
 		// NoOp, all is well.
 	}
 }
+
+func TestWritePermissionsApplied (t *testing.T) {
+	done := make(chan bool)
+	go RunServer(done, os.Getenv("PORT"))
+	defer func() { done <- true }()
+
+	ak1 := &AccessKey{
+		Read: []string{},
+		Write: []string{},
+		Token: "blah",
+	}
+
+	ak1.Save()
+
+	ak2 := &AccessKey{
+		Read: []string{"test-channel"},
+		Write: []string{},
+		Token: "bleh",
+	}
+
+	ak2.Save()
+
+	url1 := fmt.Sprintf("ws://localhost:%v/%v", os.Getenv("PORT"), ak1.Token)
+	url2 := fmt.Sprintf("ws://localhost:%v/%v", os.Getenv("PORT"), ak2.Token)
+
+	ws1, err := websocket.Dial(url1, "", "http://localhost"); if err != nil {
+		t.Fatal(err)
+	}
+
+	ws2, err := websocket.Dial(url2, "", "http://localhost"); if err != nil {
+		t.Fatal(err)
+	}
+
+	channel := make(chan Message, 1)
+
+	go func() {
+		receivedMsg := Message{}
+		err := websocket.JSON.Receive(ws2, &receivedMsg); if err != nil {
+			t.Fatal(err)
+		}
+
+		channel <- receivedMsg
+	}()
+
+
+	originalMessage := &Message{
+		Channel: "test-channel",
+		Data: "hey there!",
+		Event: "message",
+	}
+
+	go func() {
+		time.Sleep(time.Second * 1)
+		websocket.JSON.Send(ws1, &originalMessage)
+	}()
+
+	select {
+	case <- channel:
+		t.Error("This message should not have been received")
+	case <- time.After(time.Second * 3):
+		// NoOp, all is good.
+	}
+}
