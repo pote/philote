c82c0379ff920e7ac8e72501d2cdfdc4aeefd1ee
clchen <ccl0326@163.com>
2018-02-28 15:57:27 +0800

    Fix data race

--- philote.go
+++ philote.go
@@ -1,77 +1,83 @@
 package main
 
 import (
-  log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
-  "github.com/gorilla/websocket"
-  "github.com/satori/go.uuid"
+	"github.com/gorilla/websocket"
+	"github.com/satori/go.uuid"
+	"sync"
 )
 
 type Philote struct {
-  ID         string
-  AccessKey  *AccessKey
-  Hive       *hive
-  ws         *websocket.Conn
+	ID        string
+	AccessKey *AccessKey
+	Hive      *hive
+	ws        *websocket.Conn
+	wsMutex   sync.Mutex
 }
 
-func NewPhilote(ak *AccessKey, ws *websocket.Conn) (*Philote) {
-  p := &Philote{
-    ws:    ws,
-    ID: uuid.NewV4().String(),
-    AccessKey: ak,
-  }
+func NewPhilote(ak *AccessKey, ws *websocket.Conn) *Philote {
+	p := &Philote{
+		ws:        ws,
+		ID:        uuid.NewV4().String(),
+		AccessKey: ak,
+		wsMutex:   sync.Mutex{},
+	}
 
-  return p
+	return p
 }
 
 func (p *Philote) Listen() {
-  log.WithFields(log.Fields{"philote": p.ID}).Debug("Listening to Philote")
-  for {
-    message := &Message{}
-    err := p.ws.ReadJSON(&message); if err != nil {
-      log.WithFields(log.Fields{
-        "philote": p.ID,
-        "error": err.Error()}).Warn("Error reading from socket, disconnecting")
+	log.WithFields(log.Fields{"philote": p.ID}).Debug("Listening to Philote")
+	for {
+		message := &Message{}
+		err := p.ws.ReadJSON(&message)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"philote": p.ID,
+				"error":   err.Error()}).Warn("Error reading from socket, disconnecting")
 
-      p.Hive.Disconnect <- p
-      break
-    }
+			p.Hive.Disconnect <- p
+			break
+		}
 
-    // Ensure no tampering with message data
-    message.IssuerID = p.ID
+		// Ensure no tampering with message data
+		message.IssuerID = p.ID
 
-    log.WithFields(log.Fields{"philote": p.ID, "channel": message.Channel}).Debug("Received message from socket")
+		log.WithFields(log.Fields{"philote": p.ID, "channel": message.Channel}).Debug("Received message from socket")
 
-    if p.AccessKey.CanWrite(message.Channel) {
-      go p.publish(message)
-    } else {
-      log.WithFields(log.Fields{
-        "philote": p.ID,
-        "channel": message.Channel,
-        "data": message.Data,
-      }).Info("Message dropped due to insufficient write permissions")
-    }
-  }
+		if p.AccessKey.CanWrite(message.Channel) {
+			go p.publish(message)
+		} else {
+			log.WithFields(log.Fields{
+				"philote": p.ID,
+				"channel": message.Channel,
+				"data":    message.Data,
+			}).Info("Message dropped due to insufficient write permissions")
+		}
+	}
 }
 
 func (p *Philote) disconnect() {
-  log.WithFields(log.Fields{"philote": p.ID}).Debug("Closing Philote")
-  p.ws.Close()
+	log.WithFields(log.Fields{"philote": p.ID}).Debug("Closing Philote")
+	p.ws.Close()
 }
 
 func (p *Philote) publish(message *Message) {
-  message.IssuerID = p.ID
+	message.IssuerID = p.ID
 
-  for _, philote := range p.Hive.Philotes {
-    if p.ID == philote.ID {
-      continue
-    }
+	for _, philote := range p.Hive.Philotes {
+		if p.ID == philote.ID {
+			continue
+		}
 
-    for _, channel := range philote.AccessKey.Read {
-      if message.Channel == channel {
-        philote.ws.WriteJSON(message)
-        break
-      }
-    }
-  }
+		for _, channel := range philote.AccessKey.Read {
+			if message.Channel == channel {
+				philote.wsMutex.Lock()
+				philote.ws.WriteJSON(message)
+				philote.wsMutex.Unlock()
+				break
+			}
+		}
+	}
 }
